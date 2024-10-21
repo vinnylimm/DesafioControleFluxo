d4d0c0224df24265444400efc2f062455891d634
vinnylimm <vinny.mvfl12@gmail.com>
2024-10-21 17:46:12 -0300

    add code from eclipse

---
+++ src/ParametrosInvalidosException.java
@@ -0,0 +1,10 @@
+import java.io.ObjectInputStream;
+
+public class ParametrosInvalidosException extends Exception{
+	private static final long serialVersionUID = 1L;
+
+	public ParametrosInvalidosException(String msg) {
+		super(msg);
+	}
+
+}
